4652fd234e3e372dee3dbaf999b901bc39879e45
creogenus <scherbinag2001@gmail.com>
2020-11-14 20:51:50 +0200

    Main Classes

---
+++ GreenEnergy/GreenEnergy/Efficiency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenEnergy
+{
+    public class Efficiency
+    {
+        ElectricStation estation;
+        Region region;
+
+        public Efficiency(ElectricStation estation, Region region)
+        {
+            this.estation = estation;
+            this.region = region;     
+        }
+
+        public ElectricStation [] CalculateMostEfficientStation()
+        {
+            ElectricStation[] top_stations = new ElectricStation[10];
+            ElectricStation[] all_stations = ElectricStation.CreateConfigurations(estation.power,estation.budget);
+            // temp method of calculation
+
+            for(int i = 0;  i < all_stations.Length; i++)
+            {
+                all_stations[i].efficiency = all_stations[i].full_power / (all_stations[i].full_cost * region.average_temperature * 0.7 * region.humidity * 0.3 * region.intensity * 0.1 * region.windiness * 0.03);
+            }
+           
+            //need sort function
+            //
+            //
+            //
+            //
+            //
+            return top_stations;
+        }
+    }
+}
---
+++ GreenEnergy/GreenEnergy/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenEnergy
+{
+    public class User
+    {
+        double budget;
+        double power;
+        Region region;
+        string chosen_region;
+
+        public User(Region rg)
+        {
+            region = rg;
+            chosen_region = rg.GetName();
+            //не хвататет выбранных значений для бюджета и мощности
+        }
+        
+        public User(double bdgt, Region rg)
+        {
+            double budget = bdgt;
+            region = rg;
+            chosen_region = rg.GetName();
+            //не хвататет выбранного значения для мощности
+        }
+
+        public User(Region rg, double pwr)
+        {
+            power = pwr;
+            region = rg;
+            chosen_region = rg.GetName();
+            //не хвататет выбранного значения для бюджета
+        }
+
+        public User(double bdgt, double pwr, Region rg)
+        {
+            budget = bdgt;
+            power = pwr;
+            region = rg;
+            chosen_region = rg.GetName();
+        }
+
+    }
+}
