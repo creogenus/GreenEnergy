e83a0eb661b2b4d80074f4e449b692cdc4636491
Carrolit <74235265+Carrolit@users.noreply.github.com>
2020-12-14 21:01:34 +0200

    Update site.js

--- GrenEnergy(RazorPages)/test_map/wwwroot/js/site.js
+++ GrenEnergy(RazorPages)/test_map/wwwroot/js/site.js
@@ -1,18 +1,34 @@
-﻿function test_K()
-{
-    $.ajax
-         ({
-             type: "POST",
-             contentType: "application/json; charset=utf-8",
-             url: "IndexModel.aspx/GetTest",
-             success: (function (data) {
-                 $('.description').html(data);
-             }),
-             error: (function () {
-                 alert("Error occurred in server!");
-             })
-         });
+﻿function refreshDesc() {
+    $('.description').html("");   
+    $('.for_config').html("");
 }
 
-function Call_fun(f)
-{ i=f;}
+function findRegion(id) {
+    var name = id;
+    $.ajax({
+        type: 'GET',
+        url: '?handler=FindRaion',
+        data: { name },
+        success: function (data) {
+            $('.for_config').html(data);
+        },
+        error: function (error) {
+            alert("Error: " + error);
+        }
+    })
+}
+
+function findUsers(id) {
+    var name = id;
+    $.ajax({
+        type: 'GET',
+        url: '?handler=FindUser',
+        data: { name },
+        success: function (data) {
+            $('.description').html(data);
+        },
+        error: function (error) {
+            alert("Error: " + error);
+        }
+    })
+}
